d13cb6779885d42c459824ade9720054cb6e872d
Babatunde Ojo <ojo@zap.africa>
2025-01-15 20:07:07 +0100

    update config

--- src/fixtures/menu.ts
+++ src/fixtures/menu.ts
@@ -1,7 +1,7 @@
 import { IMenu } from "../interface";
 
 export const menus: IMenu[] = [
-  { name: "Wallets", link: "/wallets" },
+  { name: "Wallets", link: "/" },
   { name: "Prices", link: "/prices" },
   { name: "Peer2Peer", link: "/peer2peer" },
   { name: "Activity", link: "/activity" },
